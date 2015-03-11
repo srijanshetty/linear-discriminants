4dc1aa29cd722166494df8e279cefe23563c8eee
Srijan R Shetty <srijan.shetty@gmail.com>
2015-03-11 08:54:32 +0530

    Systematic approach for fitting Gaussians

--- gaussian.r
+++ gaussian.r
@@ -8,3 +8,19 @@ library(mixtools)
 # w1_gaussian = mvnormalmixEM(as.matrix(w1_train), k = 3, epsilon = 0.1, maxit = 100, verb = TRUE)
 # w2_gaussian = mvnormalmixEM(as.matrix(w2_train), k = 3, epsilon = 0.1, maxit = 100, verb = TRUE)
 # w3_gaussian = mvnormalmixEM(as.matrix(w3_train), k = 3, epsilon = 0.1, maxit = 100, verb = TRUE)
+
+## Here we fit a gaussian to each of the classes w1, w2, w3 using a systematic approach
+sigma = var(training_set)
+mu = apply(training_set, 2, mean)
+variance = apply(training_set, 2, var)
+
+# Initially all the means are the same
+w1_mu = w2_mu = w3_mu = mu
+
+for (c1 in seq(from = 0, to = 1, by = 0.1)) {
+	for (c2 in seq(from = 0, to = 1, by = 0.1)) {
+		for (c3 in seq(from = 0, to = 1, by = 0.1)) {
+		}
+	}
+}
+
