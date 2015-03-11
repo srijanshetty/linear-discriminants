c98b969843b855b459ea62d9f794745fe9f5284e
Srijan R Shetty <srijan.shetty@gmail.com>
2015-03-11 08:53:59 +0530

    EM algoritm for a mixture of Gaussians

--- gaussian.r
+++ gaussian.r
@@ -4,7 +4,7 @@ setwd(".")
 # Mixtools will be used to compute parameters for Gaussian
 library(mixtools)
 
-# Fit to a mixture of Gaussians
-w1_gaussian = mvnormalmixEM(as.matrix(w1_train), k = 3, epsilon = 0.1, maxit = 100, verb = TRUE)
+# We fit a mixture of three gaussians to each of the classes w1, w2, w3 using EM method
+# w1_gaussian = mvnormalmixEM(as.matrix(w1_train), k = 3, epsilon = 0.1, maxit = 100, verb = TRUE)
 # w2_gaussian = mvnormalmixEM(as.matrix(w2_train), k = 3, epsilon = 0.1, maxit = 100, verb = TRUE)
-# w3_gaussian = mvnormalmixEM(as.matrix(w2_train), k = 3, epsilon = 0.1, maxit = 100, verb = TRUE)
+# w3_gaussian = mvnormalmixEM(as.matrix(w3_train), k = 3, epsilon = 0.1, maxit = 100, verb = TRUE)
