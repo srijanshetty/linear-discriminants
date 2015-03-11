ccd0e42801f0f6aa8342bdb43eaa1bee17fca1ea
Srijan R Shetty <srijan.shetty@gmail.com>
2015-03-11 18:05:34 +0530

    LDA is self contained

--- lda.r
+++ lda.r
@@ -1,13 +1,30 @@
-setwd(".")
+# Always generate the same data
+set.seed(111)
 
+# Number of samples
+sample_size = 3000
+training_size = 0.9 * sample_size
+test_size = 0.1 * sample_size
+
+###################################################################
+# To generate the waveform data we use mlbench
+# mlbench uses Breiman's original waveform source modified for R
+library(mlbench)
+waves = mlbench.waveform(sample_size)
+data = data.frame(cbind(waves$x, waves$class))
+
+# Create train, test and validation sets for the entire data
+training_set = data[1:training_size, ]
+test_set = data[-(1:training_size), ]
+###################################################################
+
+###################################################################
+# Use LDA or QDA to classify
 # We use MASS for the lda function
 library(MASS)
 
-# Merge the training_set and validation_set
-new_training_set = rbind(training_set, validation_set)
-
 # Classify using a LDA with equal priors
-fit = lda(X22 ~ ., data = new_training_set, prior = c(1, 1, 1)/3)
+fit = lda(X22 ~ ., data = training_set, prior = c(1, 1, 1)/3)
 plot(fit)
 
 # QDA with equal priors
@@ -20,5 +37,5 @@ for (i in 1:nrow(test_set)) {
 		count = count + 1
 	}
 }
-
 rate = count / nrow(test_set)
+###################################################################
